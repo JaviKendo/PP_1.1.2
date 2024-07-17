f17dbd9a5ee81ef23d11b5bab6ceca8ca3b08c12
Javi <katajavikendo@gmail.com>
2024-07-17 19:57:07 +0500

    1 строка

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("1");
-
-        System.out.println("3");
-
-        System.out.println("5");
-
-        System.out.println("10");
-        System.out.println("10");
-        System.out.println("10");
-
-        System.out.println("11");
     }
 }
