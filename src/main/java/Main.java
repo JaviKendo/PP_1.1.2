90a327192af1821368374d97e0d63e8112ad97e0
Javi <katajavikendo@gmail.com>
2024-07-17 19:57:56 +0500

    1 строка числом

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1");
-
-        System.out.println("3");
-
-        System.out.println("11");
-
-        System.out.println("10");
-
-        System.out.println("11");
-        System.out.println(11);
+        System.out.println(1);
     }
 }
